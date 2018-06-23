ffd018dfbafe5e537a12cb7be9923efbcd27b403
Steve Lin <jojo404032@gmail.com>
2018-06-23 18:53:49 +0800

    Create Firebase Config

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -16,4 +16,40 @@
     } else {
         console.log('none');
     }
+
+    // TODO modify here to your own firebase
+    var config = {
+        apiKey: "",
+        authDomain: "<Project ID>.firebaseapp.com",
+        databaseURL: "https://<Project ID>.firebaseio.com/"
+    };
+    firebase.initializeApp(config);
+
+    var firebaseDB = firebase.database();
+
+    var userRef = firebase.database().ref('user');
+    var USER;
+    var PASSWORD;
+    userRef.on('value', function(snapshot) {
+        // console.log(snapshot.val());
+        USER = snapshot.val().id;
+        PASSWORD = snapshot.val().password;
+    });
+
+    var launchButton = document.querySelector('.launch-button');
+    var firebaseTextfield = document.querySelector('.firebase-textfield');
+    var passwordTextfield = document.querySelector('.password-textfield');
+
+    launchButton.addEventListener('click', launchButtonClicked);
+
+    function launchButtonClicked() {
+        var firebaseUser = firebaseTextfield.value;
+        var firebasePassword = passwordTextfield.value;
+
+        if(firebaseUser === USER && firebasePassword === PASSWORD) {
+            alert("Access Successed");
+        } else {
+            alert("Access Denied");
+        }
+    }
 })();
